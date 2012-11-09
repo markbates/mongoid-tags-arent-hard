11c6342d3c787a86d983736c3a0c43077f3d137c
Luke Bergen <LukeABergen@gmail.com>
2012-11-09 02:12:37 -0500

    fixing bug where adding list of tags that contains nil will raise an exception

--- spec/tags_arent_hard/tags_spec.rb
+++ spec/tags_arent_hard/tags_spec.rb
@@ -52,6 +52,10 @@ describe Mongoid::TagsArentHard::Tags do
       tags.should eql(["foo", "bar", "baz"])
     end
 
+    it "does not raise exception on nil tags" do
+      lambda {tags << nil}.should_not raise_exception
+    end
+
   end
 
   describe '+' do
